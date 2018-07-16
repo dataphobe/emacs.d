5f9b2e495161409cbcd4afab24da54fd7b90b286
dataphobe <dataphobe@datapho.be>
2018-07-16 17:47:56 +0000

    pdf view

--- wmgr.el
+++ wmgr.el
@@ -250,3 +250,19 @@
   (call-process "mpc" nil nil nil "update")
   (message "MPD Database Updated!"))
 (global-set-key (kbd "s-m u") 'mpd/update-database)
+
+
+;; ;; pdf-tools
+
+;; (use-package pdf-tools
+;;  :pin manual ;; manually update
+;;  :config
+;;  ;; initialise
+;;  (pdf-tools-install)
+;;  ;; open pdfs scaled to fit page
+;;  (setq-default pdf-view-display-size 'fit-page)
+;;  ;; automatically annotate highlights
+;;  (setq pdf-annot-activate-created-annotations t)
+;;  ;; use normal isearch
+;;  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+
